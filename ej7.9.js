3bbe1ab9b81df78083066c4ee21029105d4ce2fe
samuelnijman <snijman@alumnat.iessacolomina.es>
2024-05-17 17:02:31 +0200

    Add files via upload

---
+++ ej7.9.js
@@ -0,0 +1,12 @@
+function a() {
+    document.getElementById('Space').innerHTML = document.getElementById('Intro').innerHTML;
+}
+function b() {
+    document.getElementById('Space').innerHTML = document.getElementById('Index').innerHTML;
+}
+function c() {
+    document.getElementById('Space').innerHTML = document.getElementById('DMDE').innerHTML;
+}
+function d() {
+    document.getElementById('Space').innerHTML = document.getElementById('Funciones Básicas').innerHTML;
+}
